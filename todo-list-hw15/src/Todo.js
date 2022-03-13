77637bfc771410e4bb0b46b4b4014d1ffbf27b63
helia-chaharmir <95971806+helia-chaharmir@users.noreply.github.com>
2022-03-14 00:57:54 +0330

    Update Todo.js

--- todo-list-hw15/src/Todo.js
+++ todo-list-hw15/src/Todo.js
@@ -1,28 +1,45 @@
-import React,{useState} from 'react'
-import TodoForm from './TodoForm'
-import Todolist from './Todolist'
-import './App.css'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todolist from "./Todolist";
+import "./App.css";
 
+function Todo({ todos, removeTodo, updateTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
 
-function Todo({todos,removeTodo}) {
-    const [edit,setEdit]=useState({
-        id:null,
-        value:''
-    })
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
 
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
 
-  return todos.map((todo,index) => (
-    
-        <div className='todo-row'>
-            <input type='checkbox' className="check-box"/>
+  return todos.map((todo, index) => (
+    <div className="todo-row">
+      <input type="checkbox" className="check-box" />
 
-        <div key={todo.id} >
-            {todo.text}
-        </div>
+      <div key={todo.id}>{todo.text}</div>
 
-        <button onClick={()=>removeTodo(todo.id)} className="delet-btn">delet</button>
-        </div>
-    ));
+      <div>
+        <button onClick={() => removeTodo(todo.id)} className="delet-btn">
+          Delet
+        </button>
+        <button
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-btn"
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  ));
 }
 
-export default Todo
+export default Todo;
